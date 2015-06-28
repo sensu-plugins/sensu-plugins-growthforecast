bae529da6bc6fec0e7102c50588db41a2cd47155
Matt Jones <mattjones@yieldbot.com>
2015-06-27 22:57:32 -0400

    update repo

--- lib/sensu-plugins-growthforecast.rb
+++ lib/sensu-plugins-growthforecast.rb
@@ -1,15 +1,1 @@
-
 require 'sensu-plugins-growthforecast/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsGrowthforecast
-  class << self
-  end
-
-  class << self
-  end
-end
